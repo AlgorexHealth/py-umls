97743329a54a43859247c7cbea461b0b0f08fbee
lukeshulman <luke@algorexhealth.com>
2018-12-19 21:38:00 +0000

    Adding MRREL to the umls flow y
    Adding RREL is a bad idea but the db part is a good idea
    
    Adding query to get the cpt desc on one table

---
+++ databases/cpt_descriptions.sql
@@ -0,0 +1,44 @@
+with short_desc as (
+select
+code, 
+str as short_desc
+from mrconso
+where SAB = 'CPT' and tty='AB'),
+consumer_desc as (
+select
+code, 
+str as consumer_desc
+from mrconso
+where SAB = 'CPT' and tty='ETCF'
+),
+clinician_desc as (
+select
+code, 
+str as clinician_desc
+from mrconso
+where SAB = 'CPT' and tty='ETCLIN'
+),
+medium_descriptor as (
+select
+code, 
+str as medium_descriptor
+from mrconso
+where SAB = 'CPT' and tty='SY'),
+full_descriptor as (
+select
+code, 
+str as full_description
+from mrconso
+where SAB = 'CPT' and tty='PT'
+)
+
+SELECT 
+sd.code
+
+from short_desc sd
+    join consumer_desc cd on sd.code = cd.code
+    join clinician_desc cl_d on sd.code = cl_d.code
+    join medium_descriptor md on sd.code = md.code
+    join full_descriptor fd on sd.code = fd.code
+
+where sd.code = '99212'
